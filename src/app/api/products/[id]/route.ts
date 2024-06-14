1fd887f650275daa5c15f1b746f20d4662838ba2
neennera <neennera@gmail.com>
2024-06-14 20:10:01 +0700

    add: setup item api

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -33,18 +33,40 @@ export async function PUT(request: Request,  { params }: { params: { id: string 
         const {
             name, price, description
         } = await request.json()
-        const datas = await prisma.products.findUnique({
-            where :{
-                id
+        const priceFloat = Number(price)    
+
+        const datas = await prisma.products.update({
+            where :{ id },
+            data : {
+                name, price:priceFloat , description
             }
         })
-        if(datas === null){
-            throw new Error("no product's name in database");
-        }
         return Response.json(
             {
                 'message' : 'success',
                 'data' : datas
+            }
+        )
+        }
+    catch(error:unknown){
+        return Response.json({
+            "message" : "fail",
+            "error" : error
+        })
+    }
+}
+
+export async function DELETE(request: Request,  { params }: { params: { id: string } }){   
+    try {
+        const id = Number(params.id)  
+        const datas = await prisma.products.delete({
+            where :{
+                id
+            }
+        })
+        return Response.json(
+            {
+                'message' : 'success'
             }
         )
         }
