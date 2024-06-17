7aa2ae79a53fb436322062c04bd4e6c53b8a4398
neennera <neennera@gmail.com>
2024-06-17 13:20:02 +0700

    add: testimonial& join us

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -12,7 +12,9 @@ export default function Home() {
   return (
     <main className='flex flex-col items-center justify-between overflow-hidden p-24'>
       <div className='flex h-[90vh] flex-col items-center justify-center sm:h-[80vh]'>
-        <h1 className='font-abril text-[6.8rem] font-bold'>DiGi Market</h1>
+        <h1 className='font-abril text-[5rem] font-bold lg:text-[6.8rem]'>
+          DiGi Market
+        </h1>
         <p className='-mt-5 text-lg'>
           your one-stop marketplace for digital products.
         </p>
@@ -49,7 +51,7 @@ export default function Home() {
         <div className='mx-auto flex min-h-[85vh] flex-col items-center justify-center text-center'>
           <h2 className='mb-8 text-3xl font-bold'>What Our Users Say</h2>
           <div className='grid grid-cols-1 gap-8 text-black md:grid-cols-3'>
-            <div className='h-[300px] rounded-lg bg-white p-6 shadow-md'>
+            <div className='h-[300px] rounded-lg bg-white p-7 pt-[20%] shadow-md'>
               <p className='mb-4'>
                 "DiGi Market has transformed the way I sell my digital products.
                 The platform is user-friendly and my sales have increased
@@ -57,14 +59,14 @@ export default function Home() {
               </p>
               <div className='font-bold'>- Alice Johnson</div>
             </div>
-            <div className='h-[300px] rounded-lg bg-white p-6 shadow-md'>
+            <div className='h-[300px] rounded-lg bg-white p-7 pt-[20%] shadow-md'>
               <p className='mb-4'>
                 "I love how easy it is to find high-quality digital products on
                 DiGi Market. It's my go-to marketplace for all things digital."
               </p>
               <div className='font-bold'>- Mark Smith</div>
             </div>
-            <div className='h-[300px] rounded-lg bg-white p-6 shadow-md'>
+            <div className='h-[300px] rounded-lg bg-white p-7 pt-[20%] shadow-md'>
               <p className='mb-4'>
                 "The support team at DiGi Market is fantastic! They helped me
                 set up my shop and provided valuable tips to boost my sales."
@@ -76,27 +78,39 @@ export default function Home() {
       </section>
 
       {/* Shop Registration Section */}
-      <section className='rounded-lg bg-gray-200 px-3 py-20 text-black'>
-        <div className='container mx-auto items-center text-center'>
-          <h2 className='mb-8 text-3xl font-bold'>
-            Become a DiGi Market Seller
+      <section className='rounded-lg bg-gray-100 bg-opacity-10 px-3 py-20 text-white'>
+        <div className='container mx-auto w-[70vw] items-start pl-10 text-start'>
+          <h2 className='mb-8 text-center text-3xl font-bold'>
+            Become a DiGi Market Seller 🛍️
           </h2>
-          <p className='mb-6 text-lg'>
-            Join our marketplace and start selling your digital products to a
-            global audience.
-          </p>
-          <ul className='mx-auto max-w-lg list-inside list-disc text-left'>
-            <li className='mb-2'>Reach a global customer base</li>
-            <li className='mb-2'>Easy-to-use seller dashboard</li>
-            <li className='mb-2'>Secure payment processing</li>
-            <li className='mb-2'>Dedicated support team</li>
-            <li className='mb-2'>Promotional tools to boost your sales</li>
-          </ul>
-          <Link href='/user'>
-            <button className='mt-6 rounded-full bg-primary px-6 py-3 font-semibold text-white'>
-              Register Now
-            </button>
-          </Link>
+          <div className='flex w-full flex-row justify-between'>
+            <div>
+              <p className='mb-6 text-lg'>
+                Join our marketplace and start selling your digital products
+              </p>
+              <ul className='mx-auto ml-10 list-inside list-disc text-left'>
+                <li className='mb-2'>Reach a global customer base</li>
+                <li className='mb-2'>Easy-to-use seller dashboard</li>
+                <li className='mb-2'>Secure payment processing</li>
+                <li className='mb-2'>Dedicated support team</li>
+                <li className='mb-2'>Promotional tools to boost your sales</li>
+              </ul>
+              <Link href='/user'>
+                <button className='mt-6 w-full self-center rounded-full bg-primary py-3 font-bold text-black hover:bg-primary-dark hover:text-white'>
+                  Register Now
+                </button>
+              </Link>
+            </div>
+            <div className='hidden w-[45%] items-center justify-center lg:block'>
+              <Image
+                src='/image/joinus.jpg'
+                alt='join us'
+                width={400}
+                height={300}
+                className='rounded-lg'
+              />
+            </div>
+          </div>
         </div>
       </section>
     </main>
