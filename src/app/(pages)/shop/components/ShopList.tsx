32d0770f653aa3cfcdca0660e0ffb867f7eff493
neennera <neennera@gmail.com>
2024-06-19 14:59:38 +0700

    add: category in shop show cast

--- src/app/(pages)/shop/components/ShopList.tsx
+++ src/app/(pages)/shop/components/ShopList.tsx
@@ -1,8 +1,9 @@
-import Link from "next/link";
-import React from "react";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Loading from "@/_components/Loading";
+import Link from 'next/link';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Loading from '@/_components/Loading';
+import { ItemCategory } from '@prisma/client';
 
 interface itemSchema {
   id: number;
@@ -10,15 +11,25 @@ interface itemSchema {
   price: number;
   description: string;
   createdAt: Date;
+  categoryId: number;
+}
+interface categorySchema {
+  id: number;
+  itemCategory: string;
+  color: string;
 }
 
 export default function ShopList() {
   const [items, setItems] = useState([]);
+  const [category, setCategory] = useState([]);
   const [loading, setLoading] = useState(true);
+
   const initItems = async () => {
     try {
-      const response = await axios.get("/api/products");
+      const response = await axios.get('/api/products');
       setItems(response.data.data);
+      const response2 = await axios.get('/api/category');
+      setCategory(response2.data.data);
     } catch (error) {
       console.log(error);
       setItems([]);
@@ -32,17 +43,28 @@ export default function ShopList() {
   return (
     <>
       {loading && <Loading />}
-      <div className="grid gap-5 grid-cols-2 lg:grid-cols-4 w-full">
+      {category.map((item: categorySchema) => (
+        <p>{item.itemCategory}</p>
+      ))}
+      <div className='grid-col-1 grid w-full gap-5 sm:grid-cols-2 lg:grid-cols-4'>
         {items.map((item: itemSchema) => (
           <Link key={item.id} href={`/shop/${item.id}`}>
-            <div className="bg-gray-200 relative rounded-md w-[240px] min-h-[180px] hover:bg-gray-300 text-black p-2">
-              <div className="bg-gray-400 w-full h-[80px]"></div>
-              <p className="font-bold text-xl">{item.name}</p>
-              <p className="text-md">{item.price}</p>
-              <div className="flex flex-row space-x-2">
-                <div className="bg-red-200 px-1 rounded-md">tag 1</div>
-                <div className="bg-red-200 px-1 rounded-md">tag 1</div>
+            <div className='min-h-[180px] w-[240px] space-y-2 rounded-md bg-gray-200 p-2 text-black hover:bg-gray-300'>
+              <div className='relative h-[120px] w-full bg-gray-400'>
+                <div className='absolute bottom-1 right-0.5 flex w-[40%] items-center justify-center rounded-lg bg-[#CCCCCC] text-black opacity-80'>
+                  <p>
+                    {item.categoryId === undefined ? 'no cat' : item.categoryId}
+                  </p>
+                </div>
               </div>
+              <div className='flex flex-row justify-between'>
+                <p className='text-xl font-bold'>{item.name}</p>
+                <p className='text-md self-end text-end font-semibold text-slate-600'>
+                  {item.price} ฿
+                </p>
+              </div>
+
+              <p>{item.description.slice(0, 20)}...</p>
             </div>
           </Link>
         ))}
