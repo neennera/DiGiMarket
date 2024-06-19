b1594a12cd309a16ddd9c6b38f847ac292e54c6c
neennera <neennera@gmail.com>
2024-06-19 13:27:15 +0700

    add: api for category

---
+++ src/app/api/category/route.ts
@@ -0,0 +1,73 @@
+import { PrismaClient } from "@prisma/client"
+import { Erica_One } from "next/font/google"
+const prisma = new PrismaClient()
+
+export async function GET(){
+    const datas = await prisma.itemCategory.findMany()
+    return Response.json(
+        {
+            'message' : 'success',
+            'data' : datas
+        }
+    )
+}
+
+export async function POST(request: Request){
+    try{
+       const {
+        itemCategory
+    } = await request.json()
+    const datas = await prisma.itemCategory.create({
+        data : {
+            itemCategory
+        }
+    })
+    return Response.json(
+        {
+            'message' : 'success',
+            'data' : datas
+        }
+    ) 
+    }catch(error:unknown){
+        return Response.json(
+            {
+                'message' : 'fail',
+                'error' : error
+            }
+        )
+    }
+}
+
+export async function DELETE(request: Request){
+    try{
+       const {
+        itemCategory
+    } = await request.json()
+    const datas = await prisma.itemCategory.findFirst({
+        where : {
+            itemCategory
+        }
+    })
+    if(datas == null){
+        throw new Error("No category in database")
+    }
+    await prisma.itemCategory.delete({
+        where : {
+            id: datas.id
+        }
+    })
+    return Response.json(
+        {
+            'message' : 'success',
+            'data' : datas
+        }
+    ) 
+    }catch(error:unknown){
+        return Response.json(
+            {
+                'message' : 'fail',
+                'error' : error
+            }
+        )
+    }
+}
